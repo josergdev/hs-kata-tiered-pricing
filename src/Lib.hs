dfc45c2eb9b4b1ceaaa5b1391baf9122ef7e2cd9
josergdev <joserodriguezgarcia95@gmail.com>
2022-05-29 21:05:30 +0200

    Refactor adding types

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,19 +1,37 @@
 module Lib where
 
-range :: Maybe Integer -> Maybe Integer -> Integer -> Bool
-range Nothing Nothing _ = True
-range Nothing (Just y) n = n <= y
-range (Just x) Nothing n = n >= x
-range (Just x) (Just y) n = (x <= n) && (n <= y)
+data Range = Range
+  { min :: Maybe Integer,
+    max :: Maybe Integer
+  }
+  deriving (Show)
 
-price :: [(Integer -> Bool, Integer)] -> Integer -> Integer
-price rs n = n * snd (head $ filter (\(f, p) -> f n) rs)
+inRange :: Range -> Integer -> Bool
+inRange (Range Nothing Nothing) n = True
+inRange (Range Nothing (Just y)) n = n <= y
+inRange (Range (Just x) Nothing) n = n >= x
+inRange (Range (Just x) (Just y)) n = (x <= n) && (n <= y)
 
-ranges :: [(Integer -> Bool, Integer)]
+data Tier = Tier
+  { range :: Range,
+    unitPrice :: Integer
+  }
+  deriving (Show)
+
+newtype TieredPricing = TieredPricing {tiers :: [Tier]}
+
+tierFor :: TieredPricing -> Integer -> Tier
+tierFor tp n = head $ filter (\t -> (inRange . range) t n) (tiers tp)
+
+price :: TieredPricing -> Integer -> Integer
+price tp n = n * unitPrice (tierFor tp n)
+
+ranges :: TieredPricing
 ranges =
-  [ (range (Just 1) (Just 2), 299),
-    (range (Just 3) (Just 10), 239),
-    (range (Just 11) (Just 25), 219),
-    (range (Just 26) (Just 50), 199),
-    (range (Just 51) Nothing, 149)
-  ]
+  TieredPricing
+    [ Tier (Range (Just 1) (Just 2)) 299,
+      Tier (Range (Just 3) (Just 10)) 239,
+      Tier (Range (Just 11) (Just 25)) 219,
+      Tier (Range (Just 26) (Just 50)) 199,
+      Tier (Range (Just 51) Nothing) 149
+    ]
