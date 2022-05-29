23de3719b6d0c362dc4b49f202de5f3099615164
josergdev <joserodriguezgarcia95@gmail.com>
2022-05-29 19:38:30 +0200

    add solution

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+range :: Maybe Integer -> Maybe Integer -> Integer -> Bool
+range Nothing Nothing _ = True
+range Nothing (Just y) n = n <= y
+range (Just x) Nothing n = n >= x
+range (Just x) (Just y) n = (x <= n) && (n <= y)
+
+price :: [(Integer -> Bool, Integer)] -> Integer -> Integer
+price rs n = n * snd (head $ filter (\(f, p) -> f n) rs)
+
+ranges :: [(Integer -> Bool, Integer)]
+ranges =
+  [ (range (Just 1) (Just 2), 299),
+    (range (Just 3) (Just 10), 239),
+    (range (Just 11) (Just 25), 219),
+    (range (Just 26) (Just 50), 199),
+    (range (Just 51) Nothing, 149)
+  ]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,38 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+import Lib
+import Test.HUnit
+
+testData :: [(Integer, Integer)]
+testData =
+  [ (1, 299),
+    (2, 598),
+    (3, 717),
+    (4, 956),
+    (5, 1195),
+    (11, 2409),
+    (12, 2628),
+    (13, 2847),
+    (26, 5174),
+    (27, 5373),
+    (28, 5572),
+    (50, 9950),
+    (51, 7599),
+    (52, 7748)
+  ]
+
+priceTest :: Test
+priceTest =
+  TestList $
+    map
+      ( \(n, p) ->
+          TestCase
+            ( do
+                assertEqual
+                  ("For " <> show n <> " subscriptions, price should be " <> show p)
+                  p
+                  (price ranges n)
+            )
+      )
+      testData
+
+main :: IO Counts
+main = runTestTT priceTest
